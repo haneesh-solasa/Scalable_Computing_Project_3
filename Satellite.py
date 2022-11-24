ed03f8be4836dfc87766994aa4b930734d8edb0d
haneesh-solasa <114074641+haneesh-solasa@users.noreply.github.com>
2022-11-24 21:42:01 +0000

    24th Nov - changes

--- Satellite.py
+++ Satellite.py
@@ -2,11 +2,14 @@ import argparse
 import socket
 import time
 import traceback
+import rsa
 
-ROUTER_PORT = None
+ROUTER_PORT = []
 B_CAST_PORT = 33255
-ROUTER_ADDRESS = None
+ROUTER_ADDRESS = []
+ROUTER_NAME = []
 MY_PORT = 33258
+publicKey, privateKey = rsa.newkeys(512)
 
 
 class Satellite():
@@ -22,8 +25,6 @@ class Satellite():
         socket_0.sendto(message,('<broadcast>',B_CAST_PORT))
 
     def listen_to_router(self):
-        global ROUTER_PORT
-        global ROUTER_ADDRESS
         socket4 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         hostname = socket.gethostname()
         host = socket.gethostbyname(hostname)
@@ -35,49 +36,62 @@ class Satellite():
         conn.close()
         socket4.close()
         split_receive = data.split(' ')
-        ROUTER_PORT = int(split_receive[3])
-        ROUTER_ADDRESS = split_receive[2]
+        ROUTER_PORT.append(int(split_receive[3]))
+        ROUTER_ADDRESS.append(split_receive[2])
+        ROUTER_NAME.append(split_receive[1])
         print(ROUTER_PORT)
         print(ROUTER_ADDRESS)
+        print(ROUTER_NAME)
 
-
+    def listen_broadcasting():
+        # For listening to other routers that want to join the network
+        client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,
+                                socket.IPPROTO_UDP)
+        client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        client.bind(("",B_CAST_PORT))
+        print('listening to broadcasts:')
+        new_router = False
+        while new_router == False:
+            data,_ = client.recvfrom(1024)
+            data = data.decode('utf-8')
+            data_message = data.split(' ')
+            type = data_message[0]
+            name = data_message[1]
+            host = data_message[2]
+            port = int(data_message[3])
+            if(type.lower() == 'router'):
+                ROUTER_ADDRESS.append(host)
+                ROUTER_PORT.append(int(port))
+                ROUTER_NAME.append(name)
+                new_router = True
+            else:
+                continue
 def send_interest(ship_name):
     
     #Getting info from broadcast message from router
-    router_info = {(ROUTER_ADDRESS, ROUTER_PORT)}
+    for i in len(ROUTER_ADDRESS):
+        router_info = {(ROUTER_ADDRESS[i], ROUTER_PORT[i])}
 
-    for router in router_info:
-        try:
-            socket_1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            socket_1.connect(router)
-            message = ship_name + '/' + 'temperature'
-            socket_1.send(message.encode())
-            print("Ship Name sent : Waiting for summary information of ship")
-            #acknowledgement = socket.recv(1024)
-            #print("Acknowledgement", acknowledgement.decode('utf-8'))
-            #recv_data()
-            data = socket_1.recv(1024)
-            print(data.decode('utf-8'))
-            socket_1.close()
-        except Exception as e:
-            print('Could not send acknowledgement', e)
+        for router in router_info:
+            try:
+                socket_1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                socket_1.connect(router)
+                message = ship_name + '/' + 'temperature '
+                message=message.encode()
+                message = message + publicKey.save_pkcs1('PEM')
+                socket_1.send(message)
+                print("Ship Name sent : Waiting for summary information of ship")
+                data = socket_1.recv(1024)
+                decoded_data = decrypt_msg(data)
+                print(decoded_data)
+                socket_1.close()
+            except Exception as e:
+                print('Exception Occured', e)
 
-# def recv_data():
-#     socket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     hostname = socket.gethostname()
-#     host = socket.gethostbyname(hostname)
-#     socket2.bind((host,MY_PORT))
-#     socket2.listen(5)
-#     while True:
-#         conn, addr = socket2.accept()
-#         summary = conn.recv(1024)
-#         summary_info = summary.decode('utf-8')
-#         print("Ship Summary: ",summary_info)
-#         #action = call_action(summary_info)
-
-#         #send_to_router(conn,addr[0],action)
-#         conn.close()
-#         time.sleep(100)
+def decrypt_msg(msg):
+    decoded_data = rsa.decrypt(msg,privateKey).decode()
+    return decoded_data
 
 def call_action(summary):
     return "Functionality yet to be added"
@@ -89,7 +103,7 @@ def send_to_router(conn,raddr,action):
 
     except:
         print(traceback.format_exc())
-        print('Could not send acknowledgement', Exception)
+        print('Exception Occured:', Exception)
         
 
 
@@ -115,7 +129,7 @@ def main():
                  
         send_interest(ship_name_lower)
 
-        time.sleep(50)
+        time.sleep()
 
 
 if __name__ == '__main__':
