7cba25da433c23f06a6cb03834b25a08aa29e210
Adam Ogorek <ogoreka@tcd.ie>
2022-11-26 19:00:41 +0000

    File to filter the weather data for each specific buoy

---
+++ filter_weather.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name', type=str)
+    args=parser.parse_args()
+    name = args.name
+    weather = pd.read_csv('2022.csv')
+    filtered_weather = weather[weather['ID'] == name]
+    filtered_weather.to_csv(f'{name}.csv', index=False)
+
+if __name__ == '__main__':
+    main()
